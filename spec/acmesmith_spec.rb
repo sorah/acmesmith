81fbc299c9e7522ffa8d8df040bc5dae0659ab5b
Pim Snel <pim@lingewoud.nl>
2016-12-05 17:48:35 +0100

    missing spaces after args

--- spec/acmesmith_spec.rb
+++ spec/acmesmith_spec.rb
@@ -8,17 +8,17 @@ describe Acmesmith do
 
   it 'should display help' do
     args = ["help"]
-    expect { Acmesmith::Command.start(args)}.to output(/Commands:/).to_stdout
-    expect { Acmesmith::Command.start(args)}.to output(/authz/).to_stdout
+    expect { Acmesmith::Command.start(args) }.to output(/Commands:/).to_stdout
+    expect { Acmesmith::Command.start(args) }.to output(/authz/).to_stdout
   end
 
   it 'should merge and execute post issueing hooks' do
 
     args = ["post_issue_hooks", "test.example.com", '-c', 'spec/config.mock.yml']
-    expect { Acmesmith::Command.start(args)}.to output(/Key for test\.example\.com is issued/).to_stdout
+    expect { Acmesmith::Command.start(args) }.to output(/Key for test\.example\.com is issued/).to_stdout
 
     args = ["post_issue_hooks", "admin.example.com", '-c', 'spec/config.mock.yml']
-    expect { Acmesmith::Command.start(args)}.to output(/Running touch \/tmp\/step002-admin\.example\.com/).to_stdout
+    expect { Acmesmith::Command.start(args) }.to output(/Running touch \/tmp\/step002-admin\.example\.com/).to_stdout
 
   end
 end
