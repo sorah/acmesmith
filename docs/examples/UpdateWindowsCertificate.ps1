7c6d060c9bbd3bd6547ce271b18778cc692b438c
Sorah Fukumori <her@sorah.jp>
2018-09-25 22:40:03 +0900

    UpdateWindowsCertificate: use Switch-Certificate

--- docs/examples/UpdateWindowsCertificate.ps1
+++ docs/examples/UpdateWindowsCertificate.ps1
@@ -3,17 +3,25 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 $PSDefaultParameterValues['*:ErrorAction']='Stop'
 
-Set-Location 'Cert:\'
+Set-Location -Path cert:\LocalMachine\My
 
 $region = "ap-northeast-1"
 $bucket = "your-s3-bucket"
 $prefix = "certs/"
 $cn = "example.com"
 $pfxPassword = ConvertTo-SecureString -String "your-p12-password" -AsPlainText -Force
-$appid = "{existing-uuid}" # or "{{{0}}}" -f [GUID]::NewGuid().Guid
+$lastCertificateMarkFile = "C:\AcmesmithCert.txt"
+
+if (Test-Path $lastCertificateMarkFile) {
+  $lastCertificate = Get-ChildItem -Path (Get-Content -Path $lastCertificateMarkFile)
+} else {
+  $lastCertificate = $null
+}
 
 Import-Module AWSPowershell
 
+# In powershell 4.x or earlier
+# [System.IO.FileInfo]([System.IO.Path]::GetTempFileName())
 $currentFile = New-TemporaryFile
 Read-S3Object -Region $region -BucketName $bucket -Key ("{0}{1}/current" -f $prefix,$cn) -File $currentFile
 $current =  Get-Content $currentFile
@@ -30,11 +38,21 @@ Read-S3Object -Region $region -BucketName $bucket -Key $chainKey -File $chainFil
 $cert = Import-PfxCertificate -Password $pfxPassword -FilePath $pfxFile.FullName -CertStoreLocation 'cert:\LocalMachine\My'
 Remove-Item $pfxFile
 
-$intermediate = Import-Certificate -FilePath $chainFile.FullName -CertStoreLocation 'cert:\LocalMachine\CA' 
+$intermediate = Import-Certificate -FilePath $chainFile.FullName -CertStoreLocation 'cert:\LocalMachine\CA'
 Write-Output $intermediate
 Write-Output $cert
+
+if ($lastCertificate) {
+  Write-Output "Switching"
+  if ($lastCertificate.Thumbprint -ne $cert.Thumbprint) {
+    Switch-Certificate -OldCert $lastCertificate -NewCert $cert
+  }
+}
+$cert.Thumbprint | Out-File $lastCertificateMarkFile
 
 $expiredCerts = Get-ChildItem -Path 'Cert:\LocalMachine\My' -SSLServerAuthentication -ExpiringInDays 0 -DnsName $cert.DnsNameList[0].Unicode
 $expiredCerts | Remove-Item -DeleteKey
 
+## http.sys
+$appid = "{existing-uuid}" # or "{{{0}}}" -f [GUID]::NewGuid().Guid
 netsh http update sslcert ipport=0.0.0.0:443 ("certhash={0}" -f $cert.Thumbprint) ("appid={0}" -f $appid)
