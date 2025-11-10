bed97d43535a1990702ba521e39726f41f18f173
Sorah Fukumori <her@sorah.jp>
2025-11-11 06:04:52 +0900

    client: replace term common_name with name

--- lib/acmesmith/client.rb
+++ lib/acmesmith/client.rb
@@ -30,35 +30,35 @@ module Acmesmith
       raise NotImplementedError, "Domain authorization in advance is still not available in acme-client (v2). Required authorizations will be performed when ordering certificates"
     end
 
-    def post_issue_hooks(common_name)
-      cert = storage.get_certificate(common_name)
+    def post_issue_hooks(name)
+      cert = storage.get_certificate(name)
       execute_post_issue_hooks(cert)
     end
 
     def execute_post_issue_hooks(certificate)
-      hooks = config.post_issuing_hooks(certificate.common_name)
+      hooks = config.post_issuing_hooks(certificate.name)
       return if hooks.empty?
-      puts "=> Executing post issuing hooks for CN=#{certificate.common_name}"
+      puts "=> Executing post issuing hooks for CN=#{certificate.name}"
       hooks.each do |hook|
         hook.run(certificate: certificate)
       end
       puts
     end
 
-    def certificate_versions(common_name)
-      storage.list_certificate_versions(common_name).sort
+    def certificate_versions(name)
+      storage.list_certificate_versions(name).sort
     end
 
     def certificates_list
       storage.list_certificates.sort
     end
 
-    def current(common_name)
-      storage.get_current_certificate_version(common_name)
+    def current(name)
+      storage.get_current_certificate_version(name)
     end
 
-    def get_certificate(common_name, version: 'current', type: 'text')
-      cert = storage.get_certificate(common_name, version: version)
+    def get_certificate(name, version: 'current', type: 'text')
+      cert = storage.get_certificate(name, version: version)
 
       certs = []
       case type
@@ -76,8 +76,8 @@ module Acmesmith
       certs
     end
 
-    def save_certificate(common_name, version: 'current', mode: '0600', output:, type: 'fullchain')
-      cert = storage.get_certificate(common_name, version: version)
+    def save_certificate(name, version: 'current', mode: '0600', output:, type: 'fullchain')
+      cert = storage.get_certificate(name, version: version)
       File.open(output, 'w', mode.to_i(8)) do |f|
         case type
         when 'certificate'
@@ -90,23 +90,23 @@ module Acmesmith
       end
     end
 
-    def get_private_key(common_name, version: 'current')
-      cert = storage.get_certificate(common_name, version: version)
+    def get_private_key(name, version: 'current')
+      cert = storage.get_certificate(name, version: version)
       cert.key_passphrase = certificate_key_passphrase if certificate_key_passphrase
 
       cert.private_key.to_pem
     end
 
-    def save_private_key(common_name, version: 'current', mode: '0600', output:)
-      cert = storage.get_certificate(common_name, version: version)
+    def save_private_key(name, version: 'current', mode: '0600', output:)
+      cert = storage.get_certificate(name, version: version)
       cert.key_passphrase = certificate_key_passphrase if certificate_key_passphrase
       File.open(output, 'w', mode.to_i(8)) do |f|
         f.puts(cert.private_key)
       end
     end
 
-    def save_pkcs12(common_name, version: 'current', mode: '0600', output:, passphrase:)
-      cert = storage.get_certificate(common_name, version: version)
+    def save_pkcs12(name, version: 'current', mode: '0600', output:, passphrase:)
+      cert = storage.get_certificate(name, version: version)
       cert.key_passphrase = certificate_key_passphrase if certificate_key_passphrase
       
       p12 = cert.pkcs12(passphrase)
@@ -115,15 +115,15 @@ module Acmesmith
       end
     end
 
-    def save(common_name, version: 'current', **kwargs)
-      cert = storage.get_certificate(common_name, version: version)
+    def save(name, version: 'current', **kwargs)
+      cert = storage.get_certificate(name, version: version)
       cert.key_passphrase = certificate_key_passphrase if certificate_key_passphrase
 
       SaveCertificateService.new(cert, **kwargs).perform!
     end
 
-    def autorenew(days: 30, remaining_life: nil, common_names: nil)
-      (common_names || storage.list_certificates).each do |cn|
+    def autorenew(days: 30, remaining_life: nil, names: nil)
+      (names || storage.list_certificates).each do |cn|
         puts "=> #{cn}"
         cert = storage.get_certificate(cn)
         not_after = cert.certificate.not_after.utc
@@ -139,17 +139,17 @@ module Acmesmith
         puts "   Not valid after: #{not_after} (lifetime=#{format_duration(lifetime+1)}, remaining=#{format_duration(remaining)}, #{"%0.2f" % (ratio.to_f*100)}%)"
         next unless has_to_renew
 
-        puts " * Renewing: CN=#{cert.common_name}, SANs=#{cert.sans.join(',')}"
-        order_with_private_key(cert.common_name, *cert.sans, private_key: regenerate_private_key(cert.public_key))
+        puts " * Renewing: CN=#{cert.name}, SANs=#{cert.sans.join(',')}"
+        order_with_private_key(cert.name, *cert.sans, private_key: regenerate_private_key(cert.public_key))
       end
     end
 
-    def add_san(common_name, *add_sans)
-      puts "=> reissuing CN=#{common_name} with new SANs #{add_sans.join(?,)}"
-      cert = storage.get_certificate(common_name)
+    def add_san(name, *add_sans)
+      puts "=> reissuing CN=#{name} with new SANs #{add_sans.join(?,)}"
+      cert = storage.get_certificate(name)
       sans = cert.sans + add_sans
       puts " * SANs will be: #{sans.join(?,)}"
-      order_with_private_key(cert.common_name, *sans, private_key: regenerate_private_key(cert.public_key))
+      order_with_private_key(cert.name, *sans, private_key: regenerate_private_key(cert.public_key))
     end
 
     private
